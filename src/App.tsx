0fa95635630d5a85007b3f00bcc4a9d43a842207
ashirbad <ashirbadbehera29@gmail.com>
2022-10-01 23:33:44 +0530

    🎉  initial commit

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="flex min-h-screen items-center justify-center bg-orange-100/30 font-inter">
+      <h1 className="text-3xl text-green-400">Building something cool!</h1>
+    </div>
+  );
+}
+
+export default App;
---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+const defaultTheme = require('tailwindcss/defaultTheme')
+
+module.exports = {
+  content: [
+    "./index.html",
+    "./src/**/*.{vue,js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        'inter': ['Inter', 'sans-serif', ...defaultTheme.fontFamily.sans],
+      }
+    },
+  },
+  plugins: [],
+}
